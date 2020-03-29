84adf2f172cf43425e8d8a1179c024b93631c131
Jakub Świtalski <jakub.switalski.10@gmail.com>
2020-03-29 18:29:24 +0200

    Lesson 4b: Find the biggest number

--- search.js
+++ search.js
@@ -1,1 +1,12 @@
-console.log('The biggest number is:');
+let allNumbers = [27, 83, 29, 10, 97, 82, 593, 372];
+
+let biggestNumber = 0;
+
+for (let currentNumber of allNumbers) {
+  // console.log('The current number is:', currentNumber);
+  if (currentNumber > biggestNumber) {
+    biggestNumber = currentNumber;
+  }
+}
+
+console.log('The biggest number is:', biggestNumber);
